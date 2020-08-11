64f1ccb17948fd45e47ad7b87365137bc953c7c6
Will Bolton <wilbol@ceh.ac.uk>
2020-08-11 15:43:38 +0000

    Enhance installation script
    R CMD BATCH turns out to be deprecated in favour of RScript, which has
    the additional benefits of a) not creating .Rout files, and b) logging
    to stdout instead.
    
    The packrat/src directory was missed from .gitignore in the previous
    commit, so it is added here.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-R CMD BATCH setup.R &> /dev/null
-python3 -m venv reticulate-venv
+Rscript ./setup.R
+python3 -m venv ./reticulate-venv
 source ./reticulate-venv/bin/activate
 pip install -r requirements.txt
