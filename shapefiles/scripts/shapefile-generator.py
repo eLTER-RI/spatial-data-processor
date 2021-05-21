859a742d3297260009c15dade06e4ff831f1ab92
Will Bolton <wilbol@ceh.ac.uk>
2021-05-21 11:17:00 +0100

    Fix whitespace, quotes and remove example call

--- shapefiles/scripts/shapefile-generator.py
+++ shapefiles/scripts/shapefile-generator.py
@@ -11,49 +11,50 @@ import matplotlib.pyplot as plt
 # zone_name - the column name of the zone name in admin_zones
 
 def decomposeSite(ltser_site, admin_zones, zone_id, zone_name, debug=False):
+
     # convert CRS of dataset to match admin zones
     # this will be the CRS of the output gdf
     ltser_site = ltser_site.to_crs(admin_zones.crs)
-    
+
     # check which zones intersect the LTSER site
     ltser_zones = gpd.overlay(ltser_site,admin_zones,how='intersection')
-    
+
     # add original zones for area comparison, setting correct geometry
     # this is necessary to align the comparison geometry correctly:
     # comparing straight away with admin_zones.geometry.area doesn't
     # align the rows correctly
     ltser_zones = pd.merge(ltser_zones,admin_zones,on=zone_id)
     ltser_zones = ltser_zones.set_geometry('geometry_x')
-    
+
     # add intersection area/zone area as new column
     # full_areas definition necessary because pd.merge only allows for one geoseries
     full_areas = gpd.GeoSeries(ltser_zones['geometry_y'])
     ltser_zones['intersection_ratio'] = ltser_zones.geometry.area/full_areas.area
-        
+
     # construct gdf of cropped zones + ratio of area intersection
     gdf_out = gpd.GeoDataFrame(
         {
-            'zone_id': ltser_zones[zone_id].astype("string"),
-            'zone_name': ltser_zones[zone_name+'_x'].astype("string"),
+            'zone_id': ltser_zones[zone_id].astype('string'),
+            'zone_name': ltser_zones[zone_name+'_x'].astype('string'),
             'geometry': ltser_zones['geometry_x'],
             'area_ratio': ltser_zones['intersection_ratio']
         },
         crs = ltser_zones.crs
     )
-    
+
     # optional visual check of intersection adds full geometry of zones
     if debug:
         # add and set full geometry
         gdf_out['debug_geometry'] = ltser_zones['geometry_y']
         gdf_out = gdf_out.set_geometry('debug_geometry')
-        
+
         # plot overlap - no need to return object since plots directly to (presumably) stdout
         fig, ax = plt.subplots(figsize = (10,10))
         ax.set_axis_off()
         ax.set_title('Zones (blue) intersecting LTSER site (red)')
         gdf_out.plot(ax=ax)
         ltser_site.boundary.plot(color='r',ax=ax)
-        
+
         # drop debug_geometry column so output is identical to non-debug
         gdf_out.drop(columns='debug_geometry',inplace=True)
         gdf_out = gdf_out.set_geometry('geometry')
@@ -77,9 +78,5 @@ def shapefileGen(zone_shapefile,destination,**kwargs):
     sf_out = decomposeSite(admin_zones=sf_in,**kwargs)
     print(sf_out)
     sf_out.to_file(destination)
-    
+
     return 0
-
-ltser_site = gpd.read_file('path/to/shapefile')
-
-shapefileGen('path/to/zones/shapefile','../deims/site/zone/boundaries.shp',ltser_site=ltser_site,zone_id='ID',zone_name='Name',debug=True)
