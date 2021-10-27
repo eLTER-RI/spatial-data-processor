25eca51b114bbad1fb582c05f998a22dea58477b
Will <wilbol@ceh.ac.uk>
2021-10-27 15:50:50 +0000

    Add script to clear input and output data

---
+++ clear-data.sh
@@ -0,0 +1,9 @@
+# WARNING - only run this from the root of the git
+# repository as ./clear-data.sh, as there are no checks
+# on working directory.
+#
+# remove all input and output of both workflows
+rm -fv input/wf1/*
+rm -fv input/wf2/*
+rm -fv output/wf1/*
+rm -fv output/wf2/*
